372bd9a80502cd85199ca3927da57f2bd1039189
Romane Orlando Robb <mail@romanerobb.com>
2023-02-16 15:04:09 -0500

    Updated.

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const app = express();
 
 app.get("/", function(req, res){
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=25a9793482b86fde4b8f4e9549483d09&units=metric";
+    const query = "London";
+    const apiKey = "25a9793482b86fde4b8f4e9549483d09";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
     
     https.get(url, function(response){
         console.log(response.statusCode);
@@ -14,9 +17,12 @@ app.get("/", function(req, res){
             const weatherData = JSON.parse(data)
             const temp = weatherData.main.temp
             const weatherDescription = weatherData.weather[0].description
-            res.send("The temperature in London is " + temp + "degrees Celcius.");
+            const icon = weather[0].icon
+            const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<p>The weather is currently " + weatherDescription + "<p>");
+            res.write("<h1>The temperature in London is " + temp + "degrees Celcius.<h1>");
+            res.write("<img src=" + imageURL +">");
+            res.send();
         })
     })
-
-    // res.send("The Server is up and running.")
 })
